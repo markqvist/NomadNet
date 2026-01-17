a2a789e61f5a670802d4741fff24a583c78f4ef3
Mark Qvist <mark@unsigned.io>
2026-01-17 21:39:29 +0100

    Updated versions

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 import setuptools
 
 exec(open("nomadnet/_version.py", "r").read())
@@ -30,6 +31,6 @@ setuptools.setup(
     entry_points= {
         'console_scripts': ['nomadnet=nomadnet.nomadnet:main']
     },
-    install_requires=["rns>=1.1.1", "lxmf>=0.9.4", "urwid>=2.6.16", "qrcode"],
+    install_requires=["rns>=1.1.3", "lxmf>=0.9.4", "urwid>=2.6.16", "qrcode"],
     python_requires=">=3.7",
 )
